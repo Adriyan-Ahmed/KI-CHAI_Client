b08b0cb98fa177f3635f12844ee99bcb3552c40c
Adriyan-Ahmed <adriyan.ahmed.jubayer@gmail.com>
2024-01-04 23:48:11 +0600

    Upper General Navbar Created

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar";
 
 const Main = () => {
     return (
         <>
-            <header></header>
-            <main>
+            <header className="max-w-[1920px] mx-auto">
+                <Navbar></Navbar>
+            </header>
+            <main className="max-w-[1920px] mx-auto min-h-screen">
                 <Outlet></Outlet>
             </main>
-            <footer></footer>
+            <footer className="max-w-[1920px] mx-auto"></footer>
         </>
     );
 };
